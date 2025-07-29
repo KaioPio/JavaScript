63493d6e240943e55d3575b6b1a06f95b94fb2a0
KaioPio <164043787+KaioPio@users.noreply.github.com>
2025-07-28 21:44:39 -0300

    Add files via upload

---
+++ Modulo 2. Exec 1/exercios.js
@@ -0,0 +1,18 @@
+console.log("Os numeros operadores para as funções abaixo são 25 e 5")
+const somar = (a, b) => a + b;
+
+console.log("O resultado da operação de soma: " , somar(25, 5));
+
+const subtrair = (a, b) => a - b;
+
+console.log("O resultado da operação de subtração: " , subtrair(25, 5));
+
+const mult = (a, b) => a * b;
+
+console.log("O resultado da operação de multiplicação: ", mult(25, 5));
+
+const div = (a, b) => a / b;
+
+console.log("O resultado da operação de divisão: ", div(25, 5));div
+
+ 
